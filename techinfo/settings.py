7c461ea0b66320613f0deeb651bce513ffbd1c29
mubashar <mubasharss786@gmail.com>
2019-05-02 17:14:43 +0500

    settings modified

--- techinfo/settings.py
+++ techinfo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vin0(#rb0gl&ntri2bcnoo(_==t@%(59knkl1y7^$0^zq$v51$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
